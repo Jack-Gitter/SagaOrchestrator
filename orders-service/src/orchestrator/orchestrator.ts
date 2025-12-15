e04fb5f98ab9bdcb4fc9000563e0c6001861ca6f
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-15 13:11:05 -0500

    nice

--- orders-service/src/orchestrator/orchestrator.ts
+++ orders-service/src/orchestrator/orchestrator.ts
@@ -1,3 +1,4 @@
+import { DataSource } from "typeorm";
 import { Actor, createActor, createMachine, StateMachine} from "xstate";
 
 export class OrderSagaOrchestrator {
@@ -5,14 +6,19 @@ export class OrderSagaOrchestrator {
 	
 	private sagas = new Map<number, Actor<any>>();
 
-	constructor() {}
+	constructor(private datasource: DataSource) {}
 
 
 	initializeOrderSaga(orderId: number, productId: number, quantity: number, ) {
 
 	}
 
-	private persistOrderStateAction() {}
+	private persistOrderStateAction() {
+
+		// update the state of the order in the database 
+		// write outbox message to database
+		// serialize and write the state of the state machine to the database
+	}
 
 	private reserveInventoryAction() {}
 
