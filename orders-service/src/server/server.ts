d81eb4afb919862e50527e7902c5a2641d8622b5
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-17 07:38:36 -0500

    nice!

--- orders-service/src/server/server.ts
+++ orders-service/src/server/server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express'
-import bodyParser from 'body-parser'
+import * as bodyParser from 'body-parser'
+import { HTTP_METHOD } from './types'
 
 export class Server {
 	private app: express.Express
@@ -7,12 +8,17 @@ export class Server {
 	constructor(private port: number) {
 		this.app = express()
 		this.app.use(bodyParser.json())
-		this.registerRoute('/', this.homeRoute)
+		this.registerRoute('/', HTTP_METHOD.GET, this.homeRoute)
+		this.registerRoute('/', HTTP_METHOD.POST, this.placeOrder)
 		this.listen()
 	}
 
-	private registerRoute(path: string, func: (req: express.Request, res: express.Response) => any) {
-		this.app.get(path, func)
+	private registerRoute(path: string, method: HTTP_METHOD, func: (req: express.Request, res: express.Response) => any) {
+		if (method == 'get') {
+			this.app.get(path, func)
+		} else if (method == 'post') {
+			this.app.post(path, func)
+		}
 	}
 
 	private homeRoute(req: express.Request, res: express.Response) {
@@ -20,7 +26,10 @@ export class Server {
 	}
 
 	private placeOrder(req: express.Request, res: express.Response) {
-
+		const {productId, quantity}: {productId: number, quantity: number} = req.body
+		console.log(productId)
+		console.log(quantity)
+		res.send("ok!")
 	}
 
 	private listen() {
