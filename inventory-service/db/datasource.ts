ef17cfe49bfcb61a3d8e7b2b55a5ba4811a6be3f
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-19 09:54:19 -0500

    ok

---
+++ inventory-service/db/datasource.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import { InboxMessage } from "./entities/inbox.entity";
+import { OutboxMessage } from "./entities/outbox.entity";
+
+export const datasource = new DataSource({
+	type: 'postgres',
+	host: "localhost",
+	port: Number(process.env.PG_PORT),
+	username: process.env.PG_USERNAME,
+	password: process.env.PG_PASSWORD,
+	database: process.env.PG_DATABASE,
+	entities: [InboxMessage, OutboxMessage],
+	migrations: []
+})
