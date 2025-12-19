02c5a3deeffd392e9284316827ea1f9a3b7bb1b9
Jack Gitter <jack.a.gitter@gmail.com>
2025-12-19 08:04:41 -0500

    mgr

--- orders-service/src/db/datasource.ts
+++ orders-service/src/db/datasource.ts
@@ -4,7 +4,7 @@ import { Order } from "./entities/order.entity";
 import { InboxMessage } from "./entities/inbox.entity";
 import { OutboxMessage } from "./entities/outbox.entity";
 import { OrderSagaEntity } from "./entities/saga.entity";
-import { Init1766148852922 } from "./migrations/1766148852922-init";
+import { Init1766149471070 } from "./migrations/1766149471070-init";
 
 export const datasource = new DataSource({
 	type: 'postgres',
@@ -14,5 +14,5 @@ export const datasource = new DataSource({
 	password: process.env.PG_PASSWORD,
 	database: process.env.PG_DATABASE,
 	entities: [InboxMessage, OutboxMessage, Order, OrderSagaEntity],
-	migrations: [Init1766148852922]
+	migrations: [Init1766149471070]
 })
